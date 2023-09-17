ae290bbb169a014bb39684404fc73293c2254582
Guilherme Ferreira <guilherme.ferreira28@outlook.com>
2023-09-17 10:31:12 -0300

    First commit

---
+++ src/Home.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function Home() {
+    return (
+        <div>
+          <h1>Welcome to the jungle</h1>         
+        </div>
+      );
+}
+
+export default Home;
---
+++ src/Settings.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function Settings() {
+  return (
+    <div className="Settings">
+
+        <h1>Settings</h1>
+
+    </div>
+  );
+}
+
+export default Settings;
